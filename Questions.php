83efe1e69912adf8edf72a33c6a5ec6edc25097d
dprotsenko <dprotsenko@tin.(none)>
2012-10-30 14:22:27 +0200

    edited

--- Questions.php
+++ Questions.php
@@ -1,221 +1,12 @@
-
-<!-- saved from url=(0046)http://10.1.20.16/2ndschoolSite/questions.html -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html>
+	<head>
 		<title>Questions</title>
-		<style type="text/css">#TnITTtw-tooltip-wrap {
-  position: absolute;
-  z-index: 9999;
-  font-family: Tahoma;
-  font-size: 13px;
-  line-height: normal
-}
-
-  .TnITTtw-inside-layout {
-
-  }
-
-    .TnITTtw-content {
-      
-    }</style><style type="text/css">/* Local ID — mndocdiojkckhpnhpeminpalidafeink */
-/* Web Store ID — ihmgiclibbndffejedjimfjmfoabpcke */
-
-.TnITTtw-help-selected-wrap {
-  border: 1px solid #C7C6C2;
-  box-shadow: 0 0 10px rgba(43, 43, 43, 0.325);
-  border-radius: 4px;
-  width: 231px
-}
-
-  .TnITTtw-help-inside-layout {
-    height: 133px;
-    background: #F5F5F5;
-    border-radius: 3px
-  }
-
-    .TnITTtw-left-particle {
-      border-top-left-radius: 4px;
-      border-bottom-left-radius: 4px;
-      box-shadow: inset 0 1px 0 white;
-      color: #4E4E4E;
-      text-shadow: 0 1px 0 white;
-      width: 194px;
-      display: inline-block;
-      border-right: 1px solid #D9D9D9
-    }
-
-      .TnITTtw-content-layout {
-        text-align: left;
-        margin: 10px 15px
-      }
-
-        .TnITTtw-variant-bunch-wrap {
-
-        }
-
-          .TnITTtw-variant-bunch-wrap .inside-layout {
-
-          }
-
-            .TnITTtw-main-variant-wrap {
-
-            }
-
-              .TnITTtw-main-variant {
-                font-weight: 700;
-                font-size: 15px;
-                text-shadow: 0 1px 0 rgba(255, 255, 255, 0.775);
-                display: inline-block;
-                vertical-align: top;
-                color: #2B2B2B
-              }
-
-            .TnITTtw-variants-by-pos {
-              
-            }
-
-              .TnITTtw-TnITTtw-variant-row {
-                text-shadow: 0 1px 0 rgba(255, 255, 255, 0.775)
-              }
-
-                .TnITTtw-v-pos {
-                  font-weight: 700;
-                  margin-top: 10px;
-                  font-size: 12px;
-                  color: #545454
-                }
-
-                  .TnITTtw-v-pos:first-child {
-                    margin-top: 8px
-                  }
-
-                .TnITTtw-v-closest-wrap {
-                  font-size: 13px;
-                  color: #4E4E4E
-                }
-
-                  .TnITTtw-v-item {
-                    display: inline-block
-                  }
-
-        .TnITTtw-listen-disabled {
-          opacity: 0.475;
-          cursor: default
-        }
-
-    .TnITTtw-right-particle {
-      background: rgba(255, 255, 255, 0.125);
-      border-top-right-radius: 4px;
-      border-bottom-right-radius: 4px;
-      display: inline-block;
-      width: 36px;
-      height: 133px;
-      vertical-align: top;
-      box-shadow: inset 1px 0 3px rgba(43, 43, 43, 0.05), inset 0 1px 0 #F5F5F5
-    }
-
-      .TnITTtw-sidebar {
-
-      }
-
-        .TnITTtw-sbutton {
-          position: absolute;
-          -webkit-user-select: none;
-          cursor: pointer
-        }
-
-        .TnITTtw-listen-button {
-          width: 16px;
-          height: 17px;
-          background: url(chrome-extension://ihmgiclibbndffejedjimfjmfoabpcke/resources/images/sprites/sprite.png);
-          background-position: -54px 153px;
-          margin: 10px 0 0 10px
-        }
-
-          .TnITTtw-listen-button:active, .TnITTtw-listen-disabled {
-            background-position: -54px 136px
-          }
-
-/* ========== */
-/* Scrollbars */
-
-#TnITTtw-trVisibleLayout {
-  position: relative !important;
-  overflow: hidden;
-  height: 133px;
-  width: 100%
-}
-
-#TnITTtw-tr-scrollbar {
-  position: absolute;
-  display: block;
-  width: 9px; 
-  height: 135px; 
-  font-size: 1px
-}
-
-#TnITTtw-track { 
-  position: absolute;
-  left: 185px;
-  top: -134px;
-  width: 11px;
-  height: 129px;
-  background: transparent
-}
-
-#TnITTtw-dragBar {
-  position: absolute; 
-  left: 1px;
-  top: 1px;
-  width: 5px; 
-  height: 10px;
-  background: rgba(43, 43, 43, 0.475);
-  cursor: pointer;
-  border-radius: 3px;
-  box-shadow: 0 1px 0 #FFF
-}
-
-  #TnITTtw-dragBar:hover, #TnITTtw-dragBar:active {
-    background: rgba(43, 43, 43, 0.775)
-  } 
-
-#TnITTtw-scrollbar, #TnITTtw-track, #TnITTtw-dragBar {
-  -webkit-user-select: none
-}</style><style type="text/css">.TnITTtw-arrow {
-  display: none;
-  width: 12px;
-  height: 6px;
-  background: url(chrome-extension://ihmgiclibbndffejedjimfjmfoabpcke/resources/images/sprites/sprite.png);
-  position: relative
-}
-
-.TnITTtw-top-arrow {
-  background-position: -108px 148px;
-  bottom: -1px
-}
-
-.TnITTtw-bottom-arrow {
-  background-position: -108px 154px;
-  top: -1px
-}</style><style type="text/css">#TnITTtw-tooltip-wrap {
-  position: absolute;
-  z-index: 9999;
-  font-family: Tahoma;
-  font-size: 13px;
-  line-height: normal
-}
-
-  .TnITTtw-inside-layout {
-
-  }
-
-    .TnITTtw-content {
-      
-    }</style><style type="text/css"></style><style type="text/css"></style></head><body><a href="http://cogniance.com/" target="_blank">
-			<img src="./Questions_files/logo.png" align="right" alt="Open cogniance.com">
+		<a href="http://cogniance.com/" target="_blank">
+			<img src="logo.png" align="right" alt="Open cogniance.com">
 		</a>
-		<link rel="shortcut icon" href="http://10.1.20.16/2ndschoolSite/images/favicon.ico">
-		<link href="./Questions_files/style.css" rel="stylesheet" type="text/css" media="screen">
-		
+		<link rel="shortcut icon" href="/2ndschoolSite/images/favicon.ico" />
+		<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<style>
 			SELECT {
 			 width: 400px;
@@ -249,21 +40,21 @@
 			 float:right;
 			}						
 		</style>	
-	
+	</head>
 
-	
-		<div id="tabs">
+	<body>
+		<div id = "tabs">
 			<ul>
-			<li><a href="http://10.1.20.16/2ndschoolSite/index.html">Home</a></li>
-			<li><a href="http://10.1.20.16/2ndschoolSite/topics.html">Topics</a></li>
-			<li><a href="http://10.1.20.16/2ndschoolSite/people.html">People</a></li>
-			<li><a href="http://10.1.20.16/2ndschoolSite/contactus.html">Contact us</a></li>
-			<li><a href="./Questions_files/Questions.htm">Questions</a></li>
-		</ul></div>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="topics.html">Topics</a></li>
+			<li><a href="people.html">People</a></li>
+			<li><a href="contactus.html">Contact us</a></li>
+			<li><a href="questions.html">Questions</a></li>
+		</div>
 			
 		<div id="content">
 			<div id="center" align="center">
-				<h1>Questions</h1>
+				<H1>Questions</H1>
 			</div>
 				
 			<form id="messages" align="center">
@@ -285,10 +76,10 @@
 				<input type="text" id="email">
 				<br>
 				Enter your question<br>
-				<textarea id="textarea" rows="7" cols="40" name="question" value="Enter your question" onfocus="if (this.value == &#39;Enter your question&#39;) {this.value = &#39;&#39;;}" onblur="if (this.value == &#39;&#39;) {this.value =&#39;Enter your question&#39;;}">Enter your question</textarea>
+				<textarea id="textarea" rows=7 cols=40 name="question" value="Enter your question" onfocus="if (this.value == 'Enter your question') {this.value = '';}" onblur="if (this.value == '') {this.value ='Enter your question';}">Enter your question</textarea>
 				<br>
 				<br>
-				<button style="width:100px;" class="btn" type="button" onclick="CreateDivElement();" method="post">Ask</button>
+				<button style="width:100px;" class="btn" type="button" onclick="CreateDivElement();" onclick="return validateForm();" method="post">Ask</button>
 			</form>				
 		</div>
 		
@@ -336,7 +127,6 @@
 			  }
 			}   
 		</script> 
-
 <?php 
 
 
@@ -395,8 +185,7 @@ try {
         echo "Subject: " . $value[0] . "<br/>"; 
         echo "Nickname: " . $value[1] . "<br/>"; 
         echo "Q: " . $value[2] . "<br/>";
-     if $value[3] = null  echo "fack" . "<br/>";
-else
+     
 echo "A: " . $value[3] . "<br/>";  
         echo "--------<br/>"; 
     } 
