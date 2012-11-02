bf940e090ff40777b393bce637ef80433f668d1e
Nataliia <nsemidotska@cognaince.com>
2012-11-02 17:12:25 +0200

    updated Questions.php

--- Questions.php
+++ Questions.php
@@ -1,12 +1,221 @@
-<html>
-	<head>
+
+<!-- saved from url=(0046)http://10.1.20.16/2ndschoolSite/questions.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Questions</title>
-		<a href="http://cogniance.com/" target="_blank">
-			<img src="logo.png" align="right" alt="Open cogniance.com">
+		<style type="text/css">#TnITTtw-tooltip-wrap {
+  position: absolute;
+  z-index: 9999;
+  font-family: Tahoma;
+  font-size: 13px;
+  line-height: normal
+}
+
+  .TnITTtw-inside-layout {
+
+  }
+
+    .TnITTtw-content {
+      
+    }</style><style type="text/css">/* Local ID вЂ” mndocdiojkckhpnhpeminpalidafeink */
+/* Web Store ID вЂ” ihmgiclibbndffejedjimfjmfoabpcke */
+
+.TnITTtw-help-selected-wrap {
+  border: 1px solid #C7C6C2;
+  box-shadow: 0 0 10px rgba(43, 43, 43, 0.325);
+  border-radius: 4px;
+  width: 231px
+}
+
+  .TnITTtw-help-inside-layout {
+    height: 133px;
+    background: #F5F5F5;
+    border-radius: 3px
+  }
+
+    .TnITTtw-left-particle {
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+      box-shadow: inset 0 1px 0 white;
+      color: #4E4E4E;
+      text-shadow: 0 1px 0 white;
+      width: 194px;
+      display: inline-block;
+      border-right: 1px solid #D9D9D9
+    }
+
+      .TnITTtw-content-layout {
+        text-align: left;
+        margin: 10px 15px
+      }
+
+        .TnITTtw-variant-bunch-wrap {
+
+        }
+
+          .TnITTtw-variant-bunch-wrap .inside-layout {
+
+          }
+
+            .TnITTtw-main-variant-wrap {
+
+            }
+
+              .TnITTtw-main-variant {
+                font-weight: 700;
+                font-size: 15px;
+                text-shadow: 0 1px 0 rgba(255, 255, 255, 0.775);
+                display: inline-block;
+                vertical-align: top;
+                color: #2B2B2B
+              }
+
+            .TnITTtw-variants-by-pos {
+              
+            }
+
+              .TnITTtw-TnITTtw-variant-row {
+                text-shadow: 0 1px 0 rgba(255, 255, 255, 0.775)
+              }
+
+                .TnITTtw-v-pos {
+                  font-weight: 700;
+                  margin-top: 10px;
+                  font-size: 12px;
+                  color: #545454
+                }
+
+                  .TnITTtw-v-pos:first-child {
+                    margin-top: 8px
+                  }
+
+                .TnITTtw-v-closest-wrap {
+                  font-size: 13px;
+                  color: #4E4E4E
+                }
+
+                  .TnITTtw-v-item {
+                    display: inline-block
+                  }
+
+        .TnITTtw-listen-disabled {
+          opacity: 0.475;
+          cursor: default
+        }
+
+    .TnITTtw-right-particle {
+      background: rgba(255, 255, 255, 0.125);
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+      display: inline-block;
+      width: 36px;
+      height: 133px;
+      vertical-align: top;
+      box-shadow: inset 1px 0 3px rgba(43, 43, 43, 0.05), inset 0 1px 0 #F5F5F5
+    }
+
+      .TnITTtw-sidebar {
+
+      }
+
+        .TnITTtw-sbutton {
+          position: absolute;
+          -webkit-user-select: none;
+          cursor: pointer
+        }
+
+        .TnITTtw-listen-button {
+          width: 16px;
+          height: 17px;
+          background: url(chrome-extension://ihmgiclibbndffejedjimfjmfoabpcke/resources/images/sprites/sprite.png);
+          background-position: -54px 153px;
+          margin: 10px 0 0 10px
+        }
+
+          .TnITTtw-listen-button:active, .TnITTtw-listen-disabled {
+            background-position: -54px 136px
+          }
+
+/* ========== */
+/* Scrollbars */
+
+#TnITTtw-trVisibleLayout {
+  position: relative !important;
+  overflow: hidden;
+  height: 133px;
+  width: 100%
+}
+
+#TnITTtw-tr-scrollbar {
+  position: absolute;
+  display: block;
+  width: 9px; 
+  height: 135px; 
+  font-size: 1px
+}
+
+#TnITTtw-track { 
+  position: absolute;
+  left: 185px;
+  top: -134px;
+  width: 11px;
+  height: 129px;
+  background: transparent
+}
+
+#TnITTtw-dragBar {
+  position: absolute; 
+  left: 1px;
+  top: 1px;
+  width: 5px; 
+  height: 10px;
+  background: rgba(43, 43, 43, 0.475);
+  cursor: pointer;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 #FFF
+}
+
+  #TnITTtw-dragBar:hover, #TnITTtw-dragBar:active {
+    background: rgba(43, 43, 43, 0.775)
+  } 
+
+#TnITTtw-scrollbar, #TnITTtw-track, #TnITTtw-dragBar {
+  -webkit-user-select: none
+}</style><style type="text/css">.TnITTtw-arrow {
+  display: none;
+  width: 12px;
+  height: 6px;
+  background: url(chrome-extension://ihmgiclibbndffejedjimfjmfoabpcke/resources/images/sprites/sprite.png);
+  position: relative
+}
+
+.TnITTtw-top-arrow {
+  background-position: -108px 148px;
+  bottom: -1px
+}
+
+.TnITTtw-bottom-arrow {
+  background-position: -108px 154px;
+  top: -1px
+}</style><style type="text/css">#TnITTtw-tooltip-wrap {
+  position: absolute;
+  z-index: 9999;
+  font-family: Tahoma;
+  font-size: 13px;
+  line-height: normal
+}
+
+  .TnITTtw-inside-layout {
+
+  }
+
+    .TnITTtw-content {
+      
+    }</style><style type="text/css"></style><style type="text/css"></style></head><body><a href="http://cogniance.com/" target="_blank">
+			<img src="./Questions_files/logo.png" align="right" alt="Open cogniance.com">
 		</a>
-		<link rel="shortcut icon" href="/2ndschoolSite/images/favicon.ico" />
-		<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="shortcut icon" href="http://10.1.20.16/2ndschoolSite/images/favicon.ico">
+		<link href="./Questions_files/style.css" rel="stylesheet" type="text/css" media="screen">
+		
 		<style>
 			SELECT {
 			 width: 400px;
@@ -31,8 +240,10 @@
 			 float:left;
 			}
 			
-			#content{
-			 width:100%;
+			#content,
+			#answers{
+			 width:50%;
+			 margin: 0 auto;
 			}
 			
 			#right {
@@ -40,27 +251,27 @@
 			 float:right;
 			}						
 		</style>	
-	</head>
+	
 
-	<body>
-		<div id = "tabs">
+	
+		<div id="tabs">
 			<ul>
-			<li><a href="index.html">Home</a></li>
-			<li><a href="topics.html">Topics</a></li>
-			<li><a href="people.html">People</a></li>
-			<li><a href="contactus.html">Contact us</a></li>
-			<li><a href="Questions.php">Questions</a></li>
-		</div>
+			<li><a href="http://10.1.20.16/2ndschoolSite/index.html">Home</a></li>
+			<li><a href="http://10.1.20.16/2ndschoolSite/topics.html">Topics</a></li>
+			<li><a href="http://10.1.20.16/2ndschoolSite/people.html">People</a></li>
+			<li><a href="http://10.1.20.16/2ndschoolSite/contactus.html">Contact us</a></li>
+			<li><a href="./Questions_files/Questions.htm">Questions</a></li>
+		</ul></div>
 			
 		<div id="content">
 			<div id="center" align="center">
-				<H1>Questions</H1>
+				<h1>Questions</h1>
 			</div>
 				
-			<form id="messages" align="center">
+			<form id="messages" align="center" action="<?=$_SERVER['PHP_SELF']?>" method="post">
 				Select your subject:
 				<p>
-					<select id="subject">
+					<select id="subject" name="subject">
 						<option>Git</option>
 						<option>Jmeter</option>
 						<option>Xpath</option>
@@ -69,17 +280,17 @@
 				</p>
 				Enter your name:
 				<br>
-				<input type="text" id="username">
+				<input type="text" id="username" name="name">
 				<br>
 				Enter your email:
 				<br>
-				<input type="text" id="email">
+				<input type="text" id="email" name="email">
 				<br>
 				Enter your question<br>
-				<textarea id="textarea" rows=7 cols=40 name="question" value="Enter your question" onfocus="if (this.value == 'Enter your question') {this.value = '';}" onblur="if (this.value == '') {this.value ='Enter your question';}">Enter your question</textarea>
+				<textarea  name="question" id="textarea" rows="7" cols="40" name="question" value="Enter your question" onfocus="if (this.value == &#39;Enter your question&#39;) {this.value = &#39;&#39;;}" onblur="if (this.value == &#39;&#39;) {this.value =&#39;Enter your question&#39;;}">Enter your question</textarea>
 				<br>
 				<br>
-				<button style="width:100px;" class="btn" type="button" onclick="CreateDivElement();" onclick="return validateForm();" method="post">Ask</button>
+				<button style="width:100px;" class="btn" type="sybmit" onclick="CreateDivElement();" method="post">Ask</button>
 			</form>				
 		</div>
 		
@@ -105,35 +316,39 @@
 							alert("Enter your question!");
 							return false;
 						}
-						else {						
-							var divElement1 = document.createElement("div"); 
-							divElement1.className = "row"; 		
-							var divElement = document.createElement("div");   
-							divElement.className = "well span8";   
-							var h5Element = document.createElement("h5");
-							h5Element.innerHTML = document.getElementById("username").value;
-							var subjectElement = document.createElement("h5");
-							subjectElement.innerHTML = document.getElementById("subject").value;
-							var pElement = document.createElement("p");
-							pElement.innerHTML = document.getElementById("textarea").value; 
-							document.getElementById("messages").appendChild(divElement1);
-							divElement1.appendChild(divElement);
-							divElement.appendChild(h5Element);  
-							divElement.appendChild(subjectElement); 
-							divElement.appendChild(pElement);
-						}
 					}
 					
 			  }
+			  window.location('http://questions/questions.php');
 			}   
 		</script> 
+
 <?php 
 
+// create function for page reloading 
+function redirect($url) {
+			if(!headers_sent()) {
+				//If headers not sent yet... then do php redirect
+				header('Location: '.$url);
+				exit;
+			} else {
+				//If headers are sent... do javascript redirect... if javascript disabled, do html redirect.
+				echo '<script type="text/javascript">';
+				echo 'window.location.href="'.$url.'";';
+				echo '</script>';
+				echo '<noscript>';
+				echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+				echo '</noscript>';
+				exit;
+			}
+		}
 
 class CSV { 
 
     private $_csv_file = null; 
-
+	
+	//make redirect to clear POST variables
+	
 
     public function __construct($csv_file) { 
         if (file_exists($csv_file)) { //Если файл существует 
@@ -152,6 +367,7 @@ class CSV {
         foreach ($csv as $value) { //Проходим массив 
             //Записываем, 3-ий параметр - разделитель поля 
             fputcsv($handle, explode(";", $value), ";");  
+
         } 
         fclose($handle); //Закрываем 
     } 
@@ -175,25 +391,86 @@ class CSV {
 } 
 
 try { 
+
     $csv = new CSV("question.csv"); //Открываем наш csv 
-    /** 
+	$file = 'question.csv';
+	$id = is_file($file) ? sizeof(file($file)) : "$file file not found!";
+
+	/**
+     * Запись новой информации в CSV
+     */
+	 if ($_POST['subject'] != '' && $_POST['name'] != '' && $_POST['email'] != '' && $_POST['question'] != ''){
+			
+		
+		$subject = $_POST['subject'];
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$question = $_POST['question'];
+		$answer = '_'.$id;
+
+		$info = array($id,$subject,$name,$email,$question,$answer);
+		$info_separated =  implode(";", $info);
+		$arr = array($info_separated);
+		$csv->setCSV($arr);
+		
+	//reload to clear POST values 
+	$url = $_SERVER['HTTP_REFERER'];
+	redirect($url);
+	}
+	
+	 /** 
      * Чтение из CSV  (и вывод на экран в красивом виде) 
      */ 
+	$fname = "question.csv";
+	$fhandle = fopen($fname,"r");
+	$content = fread($fhandle,filesize($fname));
+	
+	$answer_id = $_POST['question-id'];
+	$answer_text = $_POST['answer'];
+	echo "<div id='answers'>";
     echo "<h2>Answers:</h2>"; 
     $get_csv = $csv->getCSV(); 
     foreach ($get_csv as $value) { //Проходим по строкам 
-        echo "Subject: " . $value[0] . "<br/>"; 
-        echo "Nickname: " . $value[1] . "<br/>"; 
-        echo "Q: " . $value[2] . "<br/>";
-     
-echo "A: " . $value[3] . "<br/>";  
-        echo "--------<br/>"; 
+		if ($value[1] !=''){
+			//update answer value 
+			if($value[0] == $answer_id){
+				$content = str_replace("_".$answer_id, $answer_text, $content);
+			}
+			echo "<b>Subject:</b>   " . $value[1] . "<br/>"; 
+			echo "<b>Nickname:</b>   " . $value[2] . "<br/>"; 
+			echo "<b>email:</b>   " . $value[3] . "<br/>";
+			echo "<b>Question:</b>   " . $value[4] . "<br/>"; 
+			if ($value[5] != '_'.$value[0]){
+				echo "<b>Answer:</b>   " . $value[5] . "</br>";
+			}else{
+				$url = $_SERVER['PHP_SELF'];
+				echo "<form action='$url' method='post'>
+					<input type='hidden' name='question-id' value='$value[0]' />
+					<textarea name='answer'></textarea>
+					<p>
+						<button type='submit'>Answer this question</button>
+					</p>
+					</form>";
+			}
+			echo "--------<br/><br/>"; 
+		}
     } 
+	echo "</div>";
+	$fhandle = fopen($fname,"w");
+	fwrite($fhandle,$content);
+	fclose($fhandle);
+	//make reload of page to clear POST variables 
+	if (isset($_POST['answer'])){
+		$url = $_SERVER['HTTP_REFERER'];
+		redirect($url);
+	}
+		
+}  
 
-  
-} 
 catch (Exception $e) { //Если csv файл не существует, выводим сообщение 
     echo "Ошибка: " . $e->getMessage(); 
 } 
 ?>	
+
+
 </body></html>
